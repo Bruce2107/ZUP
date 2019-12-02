1148532733e3cbadbb06e334d63467f1fcf5c49b
Eduardo Henrique da Silva <37370507+Bruce2107@users.noreply.github.com>
2019-12-02 19:15:05 -0300

    Update 01_palindromo.py

--- 01_palindromo.py
+++ 01_palindromo.py
@@ -1,4 +1,5 @@
 palavra = input("Informe uma palavra ")
+palavra = palavra.lower()
 invertida = ""
 for i in range(0, len(palavra)):  # LOOP PARA VARRER A STRING
     # CONCATENA A ULTIMA POSICAO DA STRING COM A 'INVERTIDA'  PEGANDO O ULTIMO INDICE DA 'PALAVRA'
